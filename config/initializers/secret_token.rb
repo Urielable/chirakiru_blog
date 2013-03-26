c3692b7ded31cc565dba410393d6f5611e2066e2
Daniel Martinez <eduardodaniel2@gmail.com>
2013-03-26 14:55:11 -0600

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChirakiruBlog::Application.config.secret_token = '007fe6edfb6fcdc474a7b6bfb8cf945ebf749dd6b1e9efdbb77ce85206d52df582894413b11380e54314092e0e6b0358ee2e3b9f598e7f815fd2b9d351662297'
